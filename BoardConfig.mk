d016c00af37daac053cac8a90d274180d314554a
codeworkx <daniel.hillenbrand@codeworkx.de>
2011-11-17 17:42:51 +0100

    fix path on TARGET_RECOVERY_INITRC

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -36,7 +36,7 @@ TARGET_NO_RADIOIMAGE := true
 
 TARGET_PROVIDES_INIT := true
 TARGET_PROVIDES_INIT_TARGET_RC := true
-TARGET_RECOVERY_INITRC := device/samsung/c1-common/recovery.rc
+TARGET_RECOVERY_INITRC := device/samsung/galaxys2/recovery.rc
 
 BOARD_NAND_PAGE_SIZE := 4096 -s 128
 BOARD_KERNEL_PAGESIZE := 4096
