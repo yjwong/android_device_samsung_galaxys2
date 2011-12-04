c471ca11dc5dd102559f7f6f48fd70db9b3c22d2
codeworkx <daniel.hillenbrand@codeworkx.de>
2011-12-04 16:14:44 +0100

    added cm.mk

---
+++ cm.mk
@@ -0,0 +1,20 @@
+## Specify phone tech before including full_phone
+$(call inherit-product, vendor/cm/config/gsm.mk)
+
+# Release name
+PRODUCT_RELEASE_NAME := SGS2
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/samsung/galaxys2/full_galaxys2.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := galaxys2
+PRODUCT_NAME := cm_galaxys2
+PRODUCT_BRAND := Samsung
+PRODUCT_MODEL := Galaxy S II
+
+#Set build fingerprint / ID / Prduct Name ect.
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=GT-I9100 TARGET_DEVICE=GT-I9100 BUILD_ID=GRJ22 BUILD_DISPLAY_ID=ITL41D BUILD_FINGERPRINT=samsung/GT-I9100/GT-I9100:2.3.4/GINGERBREAD/XXKG2:user/release-keys PRIVATE_BUILD_DESC="GT-I9100-user 2.3.4 GINGERBREAD XXKG2 release-keys"
