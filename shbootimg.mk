1eb034bc5652f5dc60b6a5c9a48a46ccf62dcb28
codeworkx <daniel.hillenbrand@codeworkx.de>
2011-12-19 18:55:20 +0100

    use a full kernel (zImage) until we got kernel sources

--- shbootimg.mk
+++ shbootimg.mk
@@ -4,6 +4,8 @@ INSTALLED_BOOTIMAGE_TARGET := $(PRODUCT_OUT)/boot.img
 $(INSTALLED_BOOTIMAGE_TARGET): $(TARGET_PREBUILT_KERNEL) $(recovery_ramdisk) $(INSTALLED_RAMDISK_TARGET) $(PRODUCT_OUT)/utilities/flash_image $(PRODUCT_OUT)/utilities/busybox
 	$(call pretty,"Boot image: $@")
 	$(hide) ./device/samsung/galaxys2/mkshbootimg.py $@ $(TARGET_PREBUILT_KERNEL) $(INSTALLED_RAMDISK_TARGET) $(recovery_ramdisk)
+	cp device/samsung/galaxys2/zImage $(PRODUCT_OUT)/boot.img
+	cp device/samsung/galaxys2/zImage $(PRODUCT_OUT)/recovery.img
 
 $(INSTALLED_RECOVERYIMAGE_TARGET): $(INSTALLED_BOOTIMAGE_TARGET)
 	$(ACP) $(INSTALLED_BOOTIMAGE_TARGET) $@
