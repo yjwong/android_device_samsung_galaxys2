2eceabec76723c863ba9c07fbca0beeae4e08c65
codeworkx <daniel.hillenbrand@codeworkx.de>
2011-12-25 21:39:54 +0100

    dont disable vsync

--- galaxys2_base.mk
+++ galaxys2_base.mk
@@ -138,8 +138,7 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_PROPERTY_OVERRIDES += \
 	ro.opengles.version=131072 \
-	hwui.render_dirty_regions=false \
-	hwui.disable_vsync=true
+	hwui.render_dirty_regions=false
 
 PRODUCT_PROPERTY_OVERRIDES += \
 	ro.sf.lcd_density=240
