2f84073d39417f1e0a6b1f66c1a8c953f9b976f2
XpLoDWilD <xplodgui@gmail.com>
2011-12-28 01:29:38 +0000

    Fix touchkey lights with messy samsung kernel

--- liblights/lights.c
+++ liblights/lights.c
@@ -98,6 +98,7 @@ set_light_backlight(struct light_device_t* dev,
 
     pthread_mutex_lock(&g_lock);
     err = write_int(PANEL_FILE, brightness);
+    err = write_int(BUTTON_FILE, brightness > 0 ? 1 : 0);
     pthread_mutex_unlock(&g_lock);
 
     return err;
